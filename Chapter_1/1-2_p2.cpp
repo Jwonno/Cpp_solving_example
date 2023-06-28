1c0839019e1723363c86491f900e789eea4bd6dd
saegeulu <saegeulu@naver.com>
2023-06-28 18:31:43 +0900

    챕터1-2 문제 2 해결

---
+++ Chapter_1/1-2_p2.cpp
@@ -0,0 +1,12 @@
+//아래 문장은 화면에 어떻게 출력될까요?
+#include <iostream>
+int main(){
+    std::cout << "hi" << std::endl
+              << "my name is "
+              << "Wonho" << std::endl;
+    return 0;
+}
+/*[Answer]
+hi
+my name is Wonho
+*/
