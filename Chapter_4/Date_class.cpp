683928d27861c89db6d999e864b36a7e73c351f4
Jwonno <saegeulu@naver.com>
2023-07-23 21:19:43 +0900

    생성자(Constructor) 내용 추가

--- Chapter_4/Date_class.cpp
+++ Chapter_4/Date_class.cpp
@@ -14,6 +14,14 @@ class Date{
         int GetCurrentMonthTotalDays(int year, int month);
 
         void ShowDate();
+
+        // Constructor: 객체 생성시 자동으로 호출되는 함수
+        // 객체를 초기화 하는 역할을 하기 때문에 리턴값이 없다.
+        Date(int year, int month, int day){
+            year_ = year;
+            month_ = month;
+            day_ = day;
+        }
 };
 
 void Date::SetDate(int year, int month, int day){       // Date:: 는 Date 클래스에 정의된 함수라는 뜻
@@ -62,7 +70,13 @@ void Date::ShowDate(){
 }
 
 int main(){
-    Date date;
+    // Date date(2011, 2, 1);           암시적 방법(implicit)  Date(int year, int month, int day) 를 호출한다.
+    // Date day = Date(2012, 3, 1);     명시적 방법(explicit)
+    Date date;                       // 생성자를 정의하지 않으면 디폴트 생성자(Default Constructor)가 호출된다.
+    // Date date = Date();              이렇게 명시적 방법으로 디폴트 생성자를 호출할 수도 있다. 
+    // 주의해야 할 것은 Date date(); 라고 하면 
+    // 디폴트 생성자를 이용해서 초기화하는 것이 아니라, 
+    // 리턴값이 Date 이고 인자가 없는 '함수' date 를 정의한 것
     date.SetDate(2011, 3, 1);
     date.ShowDate();
 
@@ -78,3 +92,25 @@ int main(){
 
     return 0;
 }
+
+/* 명시적으로 디폴트 생성자 사용하기 */
+class Test {
+    public:
+        Test() = default;   // 디폴트 생성자를 정의
+};
+
+/* 생성자 오버로딩 */
+class Test2 {
+    int a;
+    int b;
+    public:
+        Test2(){
+            a = 1;
+            b = 2;
+        }
+        Test2(int a_, int b_){
+            a = a_;
+            b = b_;
+        }
+};
+// 위와 같이 생성자도 오버로딩이 가능하다.
